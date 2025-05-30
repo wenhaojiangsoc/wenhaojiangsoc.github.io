46fa03f6c0d96cdbe6046800dcee1f002edb8f9a
wenhaojiangsoc <111535219+wenhaojiangsoc@users.noreply.github.com>
2025-05-30 04:33:37 +0000

    Deploying to gh-pages from @ wenhaojiangsoc/wenhaojiangsoc.github.io@bd268ade9ad359cecd4fc7bd6bcb4afb5029744d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -443,15 +443,15 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-i-presented-a-new-causal-inference-paper-cohort-discontinuity-design-with-lawrence-wu-at-paa-washington-dc",
+          title: 'I presented a new causal inference paper (cohort discontinuity design) with Lawrence Wu...',
+          description: "",
+          section: "News",},{id: "news-announcement-2",
+          title: 'Announcement_2',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
