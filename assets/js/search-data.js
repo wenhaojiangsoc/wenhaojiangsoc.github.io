de23cc50a73ef6db0f38324c60a6b289c81cb732
wenhaojiangsoc <111535219+wenhaojiangsoc@users.noreply.github.com>
2025-06-09 03:27:25 +0000

    Deploying to gh-pages from @ wenhaojiangsoc/wenhaojiangsoc.github.io@41f6c94d0e4b8d306c4f8c59263c78383e6da3ec 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "",
+          description: "My CV",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
