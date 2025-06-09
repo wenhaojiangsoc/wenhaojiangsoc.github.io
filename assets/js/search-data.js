f5c8b9afcf547fba809f8ce26196a7149e718853
wenhaojiangsoc <111535219+wenhaojiangsoc@users.noreply.github.com>
2025-06-09 03:09:57 +0000

    Deploying to gh-pages from @ wenhaojiangsoc/wenhaojiangsoc.github.io@3f3997a012095d17743d3c92d75917b4bf30290a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,21 +4,21 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-publications",
-          title: "publications",
+          title: "Publications",
           description: "A list of my recent selected publications with replication files, when available.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
           },
         },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          title: "CV",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
